9281778cff8be2a29f8e628a71fb83c27ac014d1
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2022-10-11 22:45:40 +1300

    simplify navbar

--- components/navbar.js
+++ components/navbar.js
@@ -3,6 +3,25 @@ import Link from 'next/link'
 import { useEffect } from 'react'
 
 export default function Navbar({page}) {
+  const menuItems = [
+    {
+      name: 'Home',
+      link: '/'
+    },
+    {
+      name: 'About',
+      link: '/about'
+    },
+    {
+      name: 'Photographs',
+      link: '/gallery'
+    },
+    {
+      name: 'Contact',
+      link: '/contact'
+    }
+  ]
+
   useEffect(() => {
     console.log(page)
   })
@@ -22,43 +41,22 @@ export default function Navbar({page}) {
         </Link>
         <ul
           id="menu"
-          className="w-full sm:w-auto sm:flex flex-col sm:flex-row items-center h-full py-1 pb-4 sm:py-0 sm:pb-0"
+          className="w-full sm:w-auto sm:flex flex-col sm:flex-row items-center justify-center h-full pb-5"
         >
-          <li className="sm-mr-5 text-center">
-            <Link
-              href="/"
-              aria-current="page"
-            >
-              <a
-                className={`text-white w-full no-underline sm:w-auto sm:pr-4 py-2 sm:py-1 sm:pt-2 hover:text-orange hover:underline ${page === "home" ? "text-orange" : ""}`}
+          {menuItems.map((menuItem, index) => (
+            <li key={index} className="py-2 sm-py-5 px-3 text-center">
+              <Link
+                href={menuItem.link}
+                aria-current="page"
               >
-                Home
-              </a>
-            </Link>
-          </li>
-          <li className="text-center">
-            <Link
-              href="/about"
-            >
-              <a
-                className="text-white w-full no-underline sm:w-auto sm:pr-4 py-2 sm:py-1 sm:pt-2 hover:text-orange hover:underline"
-              >
-                About
-              </a>
-              
-            </Link>
-          </li>
-          <li className="sm-ml-5 text-center">
-            <Link
-              href="/contact"
-            >
-              <a
-                className="text-white w-full no-underline sm:w-auto sm:pr-4 py-2 sm:py-1 sm:pt-2 hover:text-orange hover:underline"
-              >
-                Contact
-              </a>
-            </Link>
-          </li>
+                <a
+                  className={`text-white w-full no-underline sm:w-auto 2 hover:text-orange hover:underline ${page === "home" ? "text-orange" : ""}`}
+                >
+                  {menuItem.name}
+                </a>
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </nav>
--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home({ images }) {
       </Head>
       <section className="grid gap-5 md:grid-cols-2 md:grid-rows-2 mx-3 pl-3">
         <div className="relative border-solid border-8 border-grey-light-800">
-          <Link href="/photographs">
+          <Link href="/gallery">
           <Image
             className="photograph"
             src="https://res.cloudinary.com/cornelia-schulz-photography/image/upload/v1654638588/Cornelia%20Schulz%20Photography/51365510750_14a78ee09c_k_ue2pqq.jpg"
@@ -36,7 +36,7 @@ export default function Home({ images }) {
           />
           </Link>
           <div className="overlay absolute inset-x-0 inset-y-1/2 h-16 bg-black/50 flex justify-center items-center text-4xl">
-            <Link href="/photographs">Photographs</Link>
+            <Link href="/gallery">Photographs</Link>
           </div>
         </div>
         <div className="relative border-solid border-8 border-grey-light-800">
