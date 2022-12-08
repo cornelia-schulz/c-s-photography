63096e4bc362f3bc525f1446dd2e8a9b44efddef
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2022-12-08 13:28:40 +1300

    single image page

---
+++ components/modal.js
@@ -0,0 +1,106 @@
+import { Fragment, useState } from 'react'
+import { Transition } from '@headlessui/react'
+import Image from 'next/image'
+
+module.exports = ({
+  title = '',
+  content = '',
+  images = [],
+  image = '',
+  buttons = '',
+  classes = '',
+  onDiscard = '',
+  onConfirm = '',
+  children
+}) => {
+    let [isOpen, setIsOpen] = useState(false)
+    let [currentImage, setCurrentImage] = useState(images[image])
+
+    const handleChange = () => {
+      setIsOpen(!isOpen)
+    }
+
+    const nextImage = () => {
+        console.log('next image')
+    }
+
+    const previousImage = () => {
+        console.log('previous image')
+    }
+
+    return <>
+      <div onClick={() => handleChange()}>
+        {children}
+      </div>
+
+      <Transition show={isOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="transition-all duration-200"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-all duration-200"
+          leaveTo="opacity-0"
+          leaveFrom="opacity-100"
+        >
+          <div style={{ zIndex: '1' }} onClick={() => handleChange()} className="w-full h-full left-0 top-0 bg-black absolute" />
+        </Transition.Child>
+        <Transition.Child
+          as={Fragment}
+          enter="transition-all duration-200"
+          enterFrom="opacity-0 scale-75"
+          enterTo="opacity-100 scale-100"
+          leave="transition-all duration-200"
+          leaveTo="opacity-0 scale-75"
+          leaveFrom="opacity-100 scale-100"
+        >
+          <div style={{ zIndex: '2' }} className="flex justify-center items-center absolute left-0 top-0">
+            <div className={`w-full ${classes ? classes : 'p-4 bg-white rounded-lg'}`}>
+              <div className="w-full flex justify-between items-center mb-6">
+                <p className="font-medium text-lg">{title}</p>
+                <div onClick={() => handleChange()} className="w-8 h-8 flex justify-center items-center rounded-lg transition-all duration-200 cursor-pointer hover:bg-zinc-500/20">
+                  <svg width="24px" height="24px" viewBox="0 0 36 36" version="1.1" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+                    <path className="clr-i-outline clr-i-outline-path-1" d="M19.41,18l8.29-8.29a1,1,0,0,0-1.41-1.41L18,16.59,9.71,8.29A1,1,0,0,0,8.29,9.71L16.59,18,8.29,26.29a1,1,0,1,0,1.41,1.41L18,19.41l8.29,8.29a1,1,0,0,0,1.41-1.41Z" />
+                    <rect x={0} y={0} width={36} height={36} fillOpacity={0} />
+                  </svg>
+                </div>
+              </div>
+              <Image 
+                src={currentImage.image}
+                layout="responsive"
+                width={currentImage.landscape ? 450 : 180}
+                height={250}
+                placeholder="blur"
+                blurDataURL="/images/blur.jpg"
+              />
+              <p className="text-md mt-4">{content}</p>
+              <div className="mt-6 flex justify-end items-center gap-2">
+                {buttons.map((button, index) => (
+                  <button 
+                      onClick={() => {
+                        if(button.role === "discard") {
+                            onDiscard()
+                        }
+                        if(button.role === "confirm") {
+                            onConfirm()
+                        }
+                        if(button.role === "custom") {
+                            button.onClick()
+                        }
+                        if(button.toClose) {
+                            setIsOpen(false)
+                        }
+                      }}
+                      key={index} 
+                      className={button.classes}
+                  >
+                    {button.label}
+                  </button>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Transition.Child>
+      </Transition>
+    </>
+}
--- pages/gallery/[id].js
+++ pages/gallery/[id].js
@@ -1,9 +1,9 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import Head from 'next/head'
-import { useRouter } from 'next/router'
 import Navbar from '../../components/navbar'
 import Layout from '../../components/layout'
+import Modal from '../../components/modal'
 import { search, mapImageResources } from '../../lib/cloudinary'
 
 export async function getStaticProps(context) {
@@ -53,24 +53,31 @@ export default function Photograph({ images }) {
     <section className="grid gap-5 md:grid-cols-3">
       {images && images.map((image, index) => (
         <div key={index} className="text-center">
-          <div className="relative border-solid border-8 border-grey-light-800 mb-4">
-            <Link href="/gallery">
-              <a>
-                <Image 
-                  src={image.image}
-                  alt={image.title}
-                  layout="responsive"
-                  width={image.landscape ? 450 : 180}
-                  height={250}
-                  placeholder="blur"
-                  blurDataURL="/images/blur.jpg"
-                />
-              </a>
-            </Link>
+          <div className="border-solid border-8 border-grey-light-800 mb-4">
+            <Modal
+              title={image.title}
+              images={images}
+              image={index}
+              content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
+              onConfirm={() => console.log('Button confirm')}
+              onDiscard={() => console.log('Button discard')}
+              buttons={[
+                { role: "custom", onClick: () => console.log("custom test"), toClose: true, classes: "bg-zinc-500/20 px-4 py-2 rounded-lg hover:bg-zinc-500/30 transition-all duration-200", label: "Buy print" },
+                { role: "discard", toClose: true, classes: "bg-zinc-500/20 px-4 py-2 rounded-lg hover:bg-zinc-500/30 transition-all duration-200", label: "Close" }
+              ]}
+            >
+              <Image 
+                src={image.image}
+                alt={image.title}
+                layout="responsive"
+                width={image.landscape ? 450 : 180}
+                height={250}
+                placeholder="blur"
+                blurDataURL="/images/blur.jpg"
+              />
+            </Modal>
           </div>
-          <Link href="/gallery">
-            <a className>{image.title}</a>
-          </Link>
+          <p className>{image.title}</p>
         </div>
       ))}
     </section>
