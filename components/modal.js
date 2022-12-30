93722aa1404ba0cec87efde1dfea77cceb9044d4
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2022-12-30 14:30:53 +1300

    add cycle through images

--- components/modal.js
+++ components/modal.js
@@ -1,4 +1,4 @@
-import { Fragment, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import { Transition } from '@headlessui/react'
 import Image from 'next/image'
 
@@ -13,19 +13,39 @@ export default function Modal ({
   onConfirm = '',
   children
 }) {
-    let [isOpen, setIsOpen] = useState(false)
-    let [currentImage, setCurrentImage] = useState(images[image])
+    const [isOpen, setIsOpen] = useState(false)
+    const [currentImage, setCurrentImage] = useState(images[image])
+    const [currentIndex, setCurrentIndex] = useState(null)
+    const [isFirst, setIsFirst] = useState(false)
+    const [isLast, setIsLast] = useState(false)
+
+    useEffect(() => {
+      const current = images.findIndex(entry => {
+        return entry.id === currentImage.id
+      })
+      setCurrentIndex(current)
+      if(currentIndex === 0) setIsFirst(true)
+      if(currentIndex === images.length-1) setIsLast(true)
+    }, [currentIndex, isFirst, isLast])
 
     const handleChange = () => {
       setIsOpen(!isOpen)
     }
 
     const nextImage = () => {
-        console.log('next image')
+        if(!isLast) {
+          setCurrentImage(images[currentIndex+1])
+          setCurrentIndex(currentIndex+1)
+          setIsFirst(false)
+        }
     }
 
     const previousImage = () => {
-        console.log('previous image')
+        if(!isFirst) {
+          setCurrentImage(images[currentIndex-1])
+          setCurrentIndex(currentIndex-1)
+          setIsLast(false)
+        }
     }
 
     return <>
@@ -54,16 +74,20 @@ export default function Modal ({
           leaveTo="opacity-0 scale-75"
           leaveFrom="opacity-100 scale-100"
         >
-          <div style={{ zIndex: '2' }} className="flex justify-center items-center absolute left-0 top-0">
-            <div className={`w-full ${classes ? classes : 'p-4 bg-white rounded-lg'}`}>
+          <div style={{ zIndex: '2' }} className="flex justify-center items-center h-screen absolute left-0 top-0">
+            <div className={`w-full ${classes ? classes : 'p-4 bg-white rounded-lg relative'}`}>
               <div className="w-full flex justify-between items-center mb-6">
-                <p className="font-medium text-lg">{title}</p>
-                <div onClick={() => handleChange()} className="w-8 h-8 flex justify-center items-center rounded-lg transition-all duration-200 cursor-pointer hover:bg-zinc-500/20">
+                <h3 className="font-medium text-lg text-black">{title}</h3>
+                <div onClick={() => handleChange()} className="w-8 h-8 flex justify-center items-center rounded-lg transition-all duration-200 cursor-pointer hover:bg-dark-orange">
                   <svg width="24px" height="24px" viewBox="0 0 36 36" version="1.1" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
                     <path className="clr-i-outline clr-i-outline-path-1" d="M19.41,18l8.29-8.29a1,1,0,0,0-1.41-1.41L18,16.59,9.71,8.29A1,1,0,0,0,8.29,9.71L16.59,18,8.29,26.29a1,1,0,1,0,1.41,1.41L18,19.41l8.29,8.29a1,1,0,0,0,1.41-1.41Z" />
                     <rect x={0} y={0} width={36} height={36} fillOpacity={0} />
                   </svg>
                 </div>
+              </div>
+              <div className="absolute w-full flex justify-between top-1/2 left-0 transform -translate-y-1/2 z-10">
+                <button className={`${isFirst ? 'invisible' : 'bg-white bg-opacity-20 text-orange text-4xl ml-5 rounded-full px-4 py-1'}`} onClick={previousImage} disabled={isFirst}>&lt;</button>
+                <button className={`${isLast ? 'invisible' : 'bg-white bg-opacity-20 text-orange text-4xl mr-5 rounded-full px-4 py-1'}`} onClick={nextImage} disabled={isLast}>&gt;</button>
               </div>
               <Image 
                 alt={currentImage.title}
@@ -74,7 +98,7 @@ export default function Modal ({
                 placeholder="blur"
                 blurDataURL="/images/blur.jpg"
               />
-              <p className="text-md mt-4">{content}</p>
+              <p className="text-md mt-4 text-black">{content}</p>
               <div className="mt-6 flex justify-end items-center gap-2">
                 {buttons.map((button, index) => (
                   <button 
